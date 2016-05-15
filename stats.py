ba0cf621bf5a6cc0cc90146000c5d5cb3100e828
Ben K <Ben K>
2016-05-15 17:37:29 -0400

    peerpairs.txt

---
+++ stats.py
@@ -0,0 +1,6 @@
+print("gjdfkl")
+with open("C:\\Users\\Ben\\Documents\\peerpairs\\peerpairs.txt") as f:
+    content = f.readlines()
+    my_list = content.splitlines()
+    for line in my_list:
+        print line
